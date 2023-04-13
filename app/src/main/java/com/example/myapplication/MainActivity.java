14837dfb15cc0bcfd2d8ebb52d5953c6d0b44c3a
niquolic <nicolas.debruycker22@gmail.com>
2023-04-13 20:22:31 +0200

    access forecast datas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 import retrofit2.Call;
@@ -111,13 +112,21 @@ public class MainActivity extends AppCompatActivity {
                 double temperature = weatherData.getCurrent().getTemp_c();
                 temperatureTextView.setText(String.format("%.1f °C", temperature));
                 cityTextView.setText(weatherData.getLocation().getName());
+                WeatherData.forecast.Forecastday[] forecast = weatherData.getForecast().getForecastday();
+                Log.d("Forecast", forecast.toString());
                 // Récupération de l'URL vers l'image
                 String iconUrl = weatherData.getCurrent().getCondition().getIcon();
                 iconUrl = "https://"+iconUrl.substring(2);
                 // Affichage de l'image
                 Glide.with(MainActivity.this).load(iconUrl).into(weatherNowImageView);
-                String Forecast = String.valueOf(weatherData.getForecast());
-                Log.d("Forecast", Forecast);
+                /*WeatherData.forecast.forecastday[] forecastday = weatherData.getForecastday();
+                if (forecastday != null && forecastday.length > 0) {
+                    WeatherData.forecast.forecastday.day firstDay = forecastday[0].getDay();
+                    String conditionText = firstDay.getCondition().getText();
+                    Log.d("Condition text", conditionText);
+                } else {
+                    Log.d("Forecastday", "forecastday is null or empty");
+                }*/
 
             }
 
--- app/src/main/java/com/example/myapplication/WeatherData.java
+++ app/src/main/java/com/example/myapplication/WeatherData.java
@@ -2,20 +2,12 @@ package com.example.myapplication;
 
 //import java.util.concurrent.locks.Condition;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.lang.reflect.Array;
 import java.util.Date;
 
 public class WeatherData {
     private Location location;
     private CurrentWeather current;
     private forecast forecast;
-    private WeatherData.forecast.forecastday[] forecastday;
-    private WeatherData.forecast.forecastday.day day;
-    private WeatherData.forecast.forecastday.day.hour[] hour;
     //private DailyForecast dailyForecast;
 
     public Location getLocation() {
@@ -28,18 +20,6 @@ public class WeatherData {
 
     public forecast getForecast() {
         return forecast;
-    }
-
-    public WeatherData.forecast.forecastday[] getForecastday() {
-        return forecastday;
-    }
-
-    public WeatherData.forecast.forecastday.day getDay() {
-        return day;
-    }
-
-    public WeatherData.forecast.forecastday.day.hour[] getHour() {
-        return hour;
     }
 
     /*public DailyForecast getDailyForecast() {
@@ -211,244 +191,63 @@ public class WeatherData {
     }
 
     public static class forecast {
-        public static class forecastday {
-            public static class day {
-                public static class condition {
-                    private String text;
-                    private String icon;
+        public Forecastday[] forecastday;
 
-                    public String getText() {
-                        return text;
-                    }
+        public Forecastday[] getForecastday() {
+            return forecastday;
+        }
 
-                    public String getIcon() {
-                        return icon;
-                    }
+        public static class Forecastday {
+            private String date;
+            private ConditionDay day;
+            private Hour[] hour;
 
-                    public void setText(String text) {
-                        this.text = text;
-                    }
+            public String getDate() {
+                return date;
+            }
 
-                    public void setIcon(String icon) {
-                        this.icon = icon;
-                    }
+            public ConditionDay getDay() {
+                return day;
+            }
+
+            public Hour[] getHour() {
+                return hour;
+            }
+
+            public static class ConditionDay {
+                private String text;
+
+                public String getText() {
+                    return text;
+                }
+            }
+
+            public static class Hour {
+                private String time;
+                private double temp_c;
+                private ConditionHour condition;
+
+                public String getTime() {
+                    return time;
                 }
 
-                private double maxtemp_c;
-                private double mintemp_c;
-                private double avgtemp_c;
-                private double maxwind_kph;
-                private double totalprecip_mm;
-                private double totalprecip_in;
-                private int avgvis_km;
-                private int avghumidity;
-                private double uv;
-                private condition condition;
-
-                public double getMaxtemp_c() {
-                    return maxtemp_c;
+                public double getTemp_c() {
+                    return temp_c;
                 }
 
-                public double getMintemp_c() {
-                    return mintemp_c;
-                }
-
-                public double getAvgtemp_c() {
-                    return avgtemp_c;
-                }
-
-                public double getMaxwind_kph() {
-                    return maxwind_kph;
-                }
-
-                public double getTotalprecip_mm() {
-                    return totalprecip_mm;
-                }
-
-                public double getTotalprecip_in() {
-                    return totalprecip_in;
-                }
-
-                public int getAvgvis_km() {
-                    return avgvis_km;
-                }
-
-                public int getAvghumidity() {
-                    return avghumidity;
-                }
-
-                public double getUv() {
-                    return uv;
-                }
-
-                public condition getCondition() {
+                public ConditionHour getCondition() {
                     return condition;
                 }
+            }
 
-                public void setMaxtemp_c(double maxtemp_c) {
-                    this.maxtemp_c = maxtemp_c;
-                }
+            public static class ConditionHour {
+                private String text;
 
-                public void setMintemp_c(double mintemp_c) {
-                    this.mintemp_c = mintemp_c;
-                }
-
-                public void setAvgtemp_c(double avgtemp_c) {
-                    this.avgtemp_c = avgtemp_c;
-                }
-
-                public void setMaxwind_kph(double maxwind_kph) {
-                    this.maxwind_kph = maxwind_kph;
-                }
-
-                public void setTotalprecip_mm(double totalprecip_mm) {
-                    this.totalprecip_mm = totalprecip_mm;
-                }
-
-                public void setTotalprecip_in(double totalprecip_in) {
-                    this.totalprecip_in = totalprecip_in;
-                }
-
-                public void setAvgvis_km(int avgvis_km) {
-                    this.avgvis_km = avgvis_km;
-                }
-
-                public void setAvghumidity(int avghumidity) {
-                    this.avghumidity = avghumidity;
-                }
-
-                public void setUv(double uv) {
-                    this.uv = uv;
-                }
-
-                public void setCondition(condition condition) {
-                    this.condition = condition;
-                }
-
-                public static class hour{
-
-                    public static class condition{
-                        private String text;
-                        private String icon;
-
-                        public String getText() {
-                            return text;
-                        }
-
-                        public String getIcon() {
-                            return icon;
-                        }
-
-                        public void setText(String text) {
-                            this.text = text;
-                        }
-
-                        public void setIcon(String icon) {
-                            this.icon = icon;
-                        }
-                    }
-
-                    private Date time;
-                    private double temp_c;
-                    private double wind_kph;
-                    private double precip_mm;
-                    private double precip_in;
-                    private int humidity;
-                    private int cloud;
-                    private double feelslike_c;
-                    private double uv;
-                    private double gust_kph;
-                    private condition condition;
-
-                    public Date getTime() {
-                        return time;
-                    }
-
-                    public double getTemp_c() {
-                        return temp_c;
-                    }
-
-                    public double getWind_kph() {
-                        return wind_kph;
-                    }
-
-                    public double getPrecip_mm() {
-                        return precip_mm;
-                    }
-
-                    public double getPrecip_in() {
-                        return precip_in;
-                    }
-
-                    public int getHumidity() {
-                        return humidity;
-                    }
-
-                    public int getCloud() {
-                        return cloud;
-                    }
-
-                    public double getFeelslike_c() {
-                        return feelslike_c;
-                    }
-
-                    public double getUv() {
-                        return uv;
-                    }
-
-                    public double getGust_kph() {
-                        return gust_kph;
-                    }
-
-                    public condition getCondition() {
-                        return condition;
-                    }
-
-                    public void setTime(Date time) {
-                        this.time = time;
-                    }
-
-                    public void setTemp_c(double temp_c) {
-                        this.temp_c = temp_c;
-                    }
-
-                    public void setWind_kph(double wind_kph) {
-                        this.wind_kph = wind_kph;
-                    }
-
-                    public void setPrecip_mm(double precip_mm) {
-                        this.precip_mm = precip_mm;
-                    }
-
-                    public void setPrecip_in(double precip_in) {
-                        this.precip_in = precip_in;
-                    }
-
-                    public void setHumidity(int humidity) {
-                        this.humidity = humidity;
-                    }
-
-                    public void setCloud(int cloud) {
-                        this.cloud = cloud;
-                    }
-
-                    public void setFeelslike_c(double feelslike_c) {
-                        this.feelslike_c = feelslike_c;
-                    }
-
-                    public void setUv(double uv) {
-                        this.uv = uv;
-                    }
-
-                    public void setGust_kph(double gust_kph) {
-                        this.gust_kph = gust_kph;
-                    }
-
-                    public void setCondition(condition condition) {
-                        this.condition = condition;
-                    }
+                public String getText() {
+                    return text;
                 }
             }
         }
+
     }
 }
